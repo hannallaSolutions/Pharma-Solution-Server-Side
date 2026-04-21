329d25399d037f43a99ef56fd10f017dd03fb9bb
Mohamed2670 <mh639450@gmail.com>
2026-04-22 00:19:38 +0200

    fix logs

--- Models/Log.cs
+++ Models/Log.cs
@@ -5,12 +5,16 @@ namespace SearchTool_ServerSide.Models
     public class Log : IEntity
     {
         public int Id { get; set; }
-        public string UserEmail { get; set; }
-        public string Action { get; set; }
-        public string? Description { get; set; } = "";
-        public string IpAddress { get; set; } = "";
-        public string DeviceInfo { get; set; } = "";
-        public User User { get; set; }
+
+        public string UserEmail { get; set; } = string.Empty;
+        public string Action { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+        public string? IpAddress { get; set; }
+        public string? DeviceInfo { get; set; }
+
+        public User? User { get; set; }
+
         public DateTime Date { get; set; } = DateTime.UtcNow;
     }
 }
