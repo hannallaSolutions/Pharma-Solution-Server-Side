a6593f569b42eb4eed783335db3981afa226ddc8
Mohamed2670 <mh639450@gmail.com>
2026-05-05 00:11:57 +0300

    disable cors

--- Program.cs
+++ Program.cs
@@ -153,7 +153,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("CorsPolicy");
+//app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseMiddleware<UserLogsMiddleware>();
